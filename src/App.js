5337374fb82f2c9c1df65d74f779badf0f76ef3b
Will Ginsberg <will.j.ginsberg@gmail.com>
2019-11-10 18:52:45 -0500

    Add selection for mode

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import SpotifyApiService from './SpotifyApiService';
 
 import Tab from './Tab/Tab';
 
+const SPOTIFY_RECENT = 'SPOTIFY_RECENT';
+const SPOTIFY_TOP = 'SPOTIFY_TOP';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: SPOTIFY_TOP,
       items: [],
       page: 0
     };
@@ -45,9 +49,28 @@ class App extends React.Component {
     }
   }
 
+  componentDidUpdate(oldProps, oldState) {
+    if (this.state.mode !== oldState.mode) {
+      this.loadNextPage();
+    }
+  }
+
+  changeMode = mode => {
+    this.setState({ mode, items: [] });
+  }
+
   render() {
+
     return (
       <div>
+        <select 
+          value={this.state.mode} 
+          onChange={e => this.changeMode(e.target.value)} 
+          className="mt-8"
+        >
+          <option value={SPOTIFY_RECENT}>Your recently listened to Spotify songs</option>
+          <option value={SPOTIFY_TOP}>Your most listened to Spotify songs</option>
+        </select>
         <div className={classes.TabList}>
           {this.state.items.map((track, i) => (<Tab key={i} track={track} />))}
         </div>
