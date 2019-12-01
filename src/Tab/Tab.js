13e613234d815232686b35bfeed9faedacc02ddf
Will Ginsberg <will.j.ginsberg@gmail.com>
2019-12-01 14:48:15 -0500

    lint

--- src/Tab/Tab.js
+++ src/Tab/Tab.js
@@ -8,15 +8,15 @@ const Tab = ({ track }) => {
   const baseUrl = 'http://www.songsterr.com/a/wa/bestMatchForQueryString';
   const url = `${baseUrl}?s=${encodeURIComponent(title)}&a=${encodeURIComponent(artist)}`;
   const siteName = 'Songsterr';
-  const imageURL = track.album.images[0].url
+  const imageURL = track.album.images[0].url;
 
   return (
     <div className={classnames([classes.Tab, 'max-w-sm rounded overflow-hidden shadow-lg m-4'])}>
       <a href={url} target="_bank">
         <img alt={siteName} src={imageURL} className="w-full" />
       </a>
-        <div className="font-bold text-xl mb-2">{title}</div>
-        <div className="font-light text-xl mb-2">{artist}</div>
+      <div className="font-bold text-xl mb-2">{title}</div>
+      <div className="font-light text-xl mb-2">{artist}</div>
     </div>
   );
 };
