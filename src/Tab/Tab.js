941dbadf5ee4f2b39480c199a1bedfa42d007ba6
Will Ginsberg <will.j.ginsberg@gmail.com>
2022-08-15 11:14:24 -0400

    remove spotify

--- src/Tab/Tab.js
+++ src/Tab/Tab.js
@@ -2,15 +2,12 @@ import React from 'react';
 import classnames from 'classnames';
 import classes from './Tab.module.css';
 
-import spotifyLogo from '../assets/spotify.png';
 import ultimateGuitarLogo from '../assets/ultimate-guitar.png';
 
 const Tab = ({ track }) => {
   const title = track.name;
   const artist = track.artists[0].name;
   const imageURL = track.album.images[0].url;
-
-  const spotifyUrl = track.external_urls.spotify;
 
   const ultimateGuitarQuery = `${title} ${artist}`;
   const ultimateGuitarUrl = `https://www.ultimate-guitar.com/search.php?search_type=title&value=${encodeURIComponent(ultimateGuitarQuery)}`;
@@ -21,9 +18,6 @@ const Tab = ({ track }) => {
       <div className="font-bold text-xl mb-2">{title}</div>
       <div className="font-light text-xl mb-2">{artist}</div>
       <div className="absolute top-0 right-0 flex m-2">
-        <a href={spotifyUrl} target="_bank" className={classnames(['bg-black rounded', classes.ExternalLink])}>
-          <img alt="Play on Spotify" src={spotifyLogo} />
-        </a>
         <a href={ultimateGuitarUrl} target="_bank" className={classnames(['bg-black rounded', classes.ExternalLink])}>
           <img alt="Search for ultimate-guitar.com tabs" src={ultimateGuitarLogo} />
         </a>
